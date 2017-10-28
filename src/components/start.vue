2902e9470146aa71d9b8631f189f3857cfadf334
Seb Rejahl <sebi.rejahl@gmail.com>
2017-10-28 15:11:02 +0100

    First version

--- src/components/start.vue
+++ src/components/start.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="start">
+    <img class="logo" src="./../assets/logo.jpg">
     <h1>{{ msg }}</h1>
+    <h2>In the meantime find us on social media below...</h2>
+    <a href="https://www.facebook.com/writtenininkband/">
+      <i class="fa fa-facebook-square fa-4x social-link" aria-hidden="true"></i>
+    </a>
+    <a href="https://www.youtube.com/channel/UCja7GCil8LEOamdofCARF0g">
+      <i class="fa fa-youtube-square fa-4x social-link" aria-hidden="true"></i>
+    </a>
+    <a href="https://soundcloud.com/user-443869644">
+      <i class="fa fa-soundcloud fa-4x social-link" aria-hidden="true"></i>
+    </a>
+    <a href="https://www.instagram.com/writteninink_band/?hl=en">
+      <i class="fa fa-instagram fa-4x social-link" aria-hidden="true"></i>
+    </a>
+    <i class="fa fa-spotify fa-4x social-link-coming" aria-hidden="true"></i>
   </div>
 </template>
 
@@ -34,4 +49,16 @@ li {
 a {
   color: #42b983;
 }
+.social-link {
+  color: #0c0c0c;
+  margin: 20px;
+}
+.social-link-coming {
+  color: grey;
+  margin: 20px;
+}
+.logo {
+  margin-bottom: 60px;
+}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import start from '@/components/start'
+import VTooltip from 'v-tooltip'
 
 Vue.use(Router)
+Vue.use(VTooltip)
 
 export default new Router({
   routes: [
