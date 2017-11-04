6e906a5f7154dd26c01603198a50e6c039a40c59
Seb Rejahl <sebi.rejahl@gmail.com>
2017-11-04 13:36:38 +0000

    Navbar und Carousel

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+    <b-navbar sticky class="navbar-wii" toggleable="md">
+
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+      <b-navbar-brand href="#">
+        <img src="./assets/logo-nav.jpg"/>
+      </b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-nav is-nav-bar>
+          <b-nav-item href="#">Upcoming Events</b-nav-item>
+          <b-nav-item href="#">Our Music</b-nav-item>
+          <b-nav-item href="#social-media">Social Media</b-nav-item>
+          <b-nav-item href="#">Contact Us</b-nav-item>
+        </b-nav>
+      </b-collapse>
+    </b-navbar>
+
+
     <router-view/>
   </div>
 </template>
@@ -17,6 +37,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+.navbar-wii {
+  background-color: rgba(255, 255, 255, 0.1);
+  -webkit-box-shadow: 0px 3px 3px 0px rgba(0,0,0,0.43);
+  -moz-box-shadow: 0px 3px 3px 0px rgba(0,0,0,0.43);
+  box-shadow: 0px 3px 3px 0px rgba(0,0,0,0.43);
+}
+
+.navbar-wii .navbar-nav .nav-link {
+  color: #a21919;
+  font-size: 24px;
+  font-family: 'Permanent Marker', cursive;
+  margin-left: 30px;
 }
 </style>
